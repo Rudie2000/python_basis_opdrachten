1b2173eecb964bfa54d0bccfc2fd67d3c271f012
Rudie2000 <97050394@st.deltion.nl>
2024-02-29 15:39:24 +0100

    15:39

--- opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
+++ opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
@@ -1,9 +1,12 @@
 # Opdracht 1
 # Naam student:
 # Groep:
+my_string = "Tinus gaat op zijn tandem naar de hottentottententoonstelling"
+# De letter "t" komt 10x voor in my_string
 
 # Hier komt je code...
 
 my_list = ...
 
-print()
+print(my_string)
+print(f"De letter \"t\" komt {my_string.count("t")}x voor in my_string")
--- opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
@@ -3,3 +3,12 @@
 # Groep:
 
 # Hier komt je code...
+
+print("    *   "*  2)
+print("   ***   "*  2)
+print("  ******  "*  2)
+print(" ******** "*  2)
+print("***********"*  2)
+print("    ***    "*  2)
+print("    ***    "*  2)
+print("    ***    "*  2)
