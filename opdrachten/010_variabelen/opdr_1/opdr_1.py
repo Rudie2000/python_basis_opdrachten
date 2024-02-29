75ebe5ac55d88dab622e873f4de31f9717af6e15
Rudie2000 <97050394@st.deltion.nl>
2024-02-29 15:17:42 +0100

    29-2-2024

--- opdrachten/010_variabelen/opdr_1/opdr_1.py
+++ opdrachten/010_variabelen/opdr_1/opdr_1.py
@@ -4,9 +4,5 @@
 
 # Hier komt je code...
 
-my_list = ...
-
-
-test
-
-hasattr
+my_list = [13, "wat een weertje", 12.5, 8]
+print(my_list)
--- opdrachten/010_variabelen/opdr_3/opdr_3.py
+++ opdrachten/010_variabelen/opdr_3/opdr_3.py
@@ -3,7 +3,11 @@
 # Groep:
 
 # Hier komt je code...
+#naam = willem  
+#achternaam = van der broek  
+#l#eeftijd = 23  
+#favoriete kleur = blauw 
 
-my_dict = ...
+my_dict = {"naam": "willem", "achternaam":"van der broek","leeftijd": "23","favoriete kleur":"blauw"}
 
-print()
+print(my_dict["naam"])
