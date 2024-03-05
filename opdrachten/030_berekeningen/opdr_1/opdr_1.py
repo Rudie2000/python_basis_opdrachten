08ef8deb501b7e951c5822af2168ba42ea8aa87e
Rudie2000 <97050394@st.deltion.nl>
2024-03-05 20:55:05 +0100

    Berekeningen opdracht 1 en 2

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -3,3 +3,13 @@
 # Groep:
 
 # Hier komt je code...
+
+
+joe = 23
+bob = 15
+fred = 22
+
+print(f"Dit weten we over joe, bob en fred:\n Joe is {joe},\n bob is {bob},\n fred is {fred}.")
+print("De totale waarde van de leeftijden bij elkaar opgeteld zijn: "+str(joe + bob + fred))
+print("De gemiddelde leeftijd van deze boys is: " +str(int((joe + bob + fred) / 3)))
+
--- opdrachten/030_berekeningen/opdr_2/opdr_2.py
+++ opdrachten/030_berekeningen/opdr_2/opdr_2.py
@@ -3,8 +3,13 @@
 # Groep:
 
 # Hier komt je code...
+# F = (C x 1,8) + 32.
+# C = (F – 32) / 1,8
 
-c = ...
-f = ...
+c = -12
+f = 144
+fahrenheit = ((c * 1.8) + 32) 
+celsuis = ((f - 32) / 1.8)
 
-print()
+print(f"{c} graden Celsius is gelijk aan {fahrenheit} graden Fahrenheit")
+print(f"{f} graden Fahrenheit is gelijk aan {celsuis} graden Celsuis")
