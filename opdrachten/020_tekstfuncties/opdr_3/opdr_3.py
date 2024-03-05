08fd055b58ee5502a5c54cc6fd11a1b801630ba8
Rudie2000 <97050394@st.deltion.nl>
2024-03-05 19:40:31 +0100

    Kerstbomen gefixit

--- opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
@@ -4,11 +4,22 @@
 
 # Hier komt je code...
 
-print("    *   "*  2)
-print("   ***   "*  2)
-print("  ******  "*  2)
-print(" ******** "*  2)
-print("***********"*  2)
-print("    ***    "*  2)
-print("    ***    "*  2)
-print("    ***    "*  2)
+
+ 
+kerstboom_1 = "     *   "
+kerstboom_2= "    ***  "
+kerstboom_3= "   ******    "
+kerstboom_4= "  ********   "
+kerstboom_5= " *********** "
+kerstboom_6= "     ***     "
+kerstboom_7= "     ***     "
+kerstboom_8= "     ***     "
+
+print(f"{kerstboom_1}\t"*5)
+print(f"{kerstboom_2}\t"*5)
+print(f"{kerstboom_3}\t"*5)
+print(f"{kerstboom_4}\t"*5)
+print(f"{kerstboom_5}\t"*5)
+print(f"{kerstboom_6}\t"*5)
+print(f"{kerstboom_7}\t"*5)
+print(f"{kerstboom_8}\t"*5)
